eb54e29c38e9ad0a84daf5132cfcb6048a7b01bf
MadhanSmart <128899467+MadhanSmart@users.noreply.github.com>
2023-06-07 18:32:52 +0530

    Add files via upload

---
+++ restapi_jwttoken/src/main/java/com/jwt/restapi/payloads/response/MessageResponse.java
@@ -0,0 +1,20 @@
+package com.jwt.restapi.payloads.response;
+
+public class MessageResponse {
+
+	private String message;
+
+	public MessageResponse(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ restapi_jwttoken/src/main/java/com/jwt/restapi/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.jwt.restapi.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jwt.restapi.model.User;
+import com.jwt.restapi.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+		return UserDetailsImpl.build(user);
+	}
+
+}
